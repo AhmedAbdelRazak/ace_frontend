5f7964a6982df45b5de1f8b507190c7bebc115f7
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2023-01-11 16:09:26 -0800

    Messenger chat

--- src/index.js
+++ src/index.js
@@ -13,29 +13,30 @@ ReactDOM.render(
 				pageId={process.env.REACT_APP_FACEBOOK_PAGE_ID}
 				language='sv_SE'
 				// themeColor={"#000000"}
-				bottomSpacing={100}
+				bottomSpacing={50}
+				zIndex={10000}
 				// loggedInGreeting='loggedInGreeting'
 				// loggedOutGreeting='loggedOutGreeting'
-				greetingDialogDisplay={"show"}
+				// greetingDialogDisplay={"show"}
 				debugMode={true}
-				onMessengerShow={() => {
-					console.log("onMessengerShow");
-				}}
-				onMessengerHide={() => {
-					console.log("onMessengerHide");
-				}}
-				onMessengerDialogShow={() => {
-					console.log("onMessengerDialogShow");
-				}}
-				onMessengerDialogHide={() => {
-					console.log("onMessengerDialogHide");
-				}}
-				onMessengerMounted={() => {
-					console.log("onMessengerMounted");
-				}}
-				onMessengerLoad={() => {
-					console.log("onMessengerLoad");
-				}}
+				// onMessengerShow={() => {
+				// 	console.log("onMessengerShow");
+				// }}
+				// onMessengerHide={() => {
+				// 	console.log("onMessengerHide");
+				// }}
+				// onMessengerDialogShow={() => {
+				// 	console.log("onMessengerDialogShow");
+				// }}
+				// onMessengerDialogHide={() => {
+				// 	console.log("onMessengerDialogHide");
+				// }}
+				// onMessengerMounted={() => {
+				// 	console.log("onMessengerMounted");
+				// }}
+				// onMessengerLoad={() => {
+				// 	console.log("onMessengerLoad");
+				// }}
 			/>
 			<App />
 		</CartProvider>
